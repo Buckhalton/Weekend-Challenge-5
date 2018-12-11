77151f2e246795a5cb8d7a16c189439168c8cbe9
admin <admin@admins-MacBook-Pro.local>
2018-12-10 19:45:58 -0600

    deployed with heroku

---
+++ build/precache-manifest.0488b481077ec7c42b58042fee7b69cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d45f0c9181b17eb6641",
+    "url": "/static/css/main.745073c0.chunk.css"
+  },
+  {
+    "revision": "2d45f0c9181b17eb6641",
+    "url": "/static/js/main.2d45f0c9.chunk.js"
+  },
+  {
+    "revision": "51a2636ca4d55a666e83",
+    "url": "/static/js/1.51a2636c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1d375b8fb03106ae2ed3be707d862e1f",
+    "url": "/index.html"
+  }
+];
